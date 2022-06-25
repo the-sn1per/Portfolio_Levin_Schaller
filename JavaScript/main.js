64885580a946054ae29c8a2067f28e0b620d8e3f
the_sn1per <levinsch@bluewin.ch>
2022-06-25 16:36:03 +0200

    hamburger added

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,11 @@
 function show(){
+    target = document.getElementById("hamburger-layout")
     element = document.getElementById("navigation")
     if(element.classList.contains("active")){
         element.classList.remove("active")
+        target.classList.remove("burger-active")
         return
     }
+    target.classList.add("burger-active")
     element.classList.add("active")
 }
